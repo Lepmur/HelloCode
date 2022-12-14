0296f155fadb5954d5ba6f1689c2213f5feb0246
Sergei <ip744692@yandex.ru>
2022-12-14 21:38:38 +0300

    add Example 006 max number IF

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1;
+int b = 9;
+int c = 5;
+int d = 304;
+int e = 198;
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+Console.Write("Наибольшим является число ");
+Console.WriteLine(max);
