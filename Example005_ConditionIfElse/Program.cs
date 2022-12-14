85230474811a114ced22ec76b4975d84a99645a6
Sergei <ip744692@yandex.ru>
2022-12-14 21:18:17 +0300

    add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите ваше имя и нажмите Enter ");
+string name = Console.ReadLine();
+if (name.ToLower == "sergei") {
+    Console.Write("Здорова, Серега!!!");
+} else 
+{
+    Console.Write("Приятно познакомиться, ");
+    Console.Write(name);
+    Console.Write("! А где Серега?!");
+}
