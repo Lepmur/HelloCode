59ad9be5f3c5a0680a3dae9e5ac1e9c2cafc50cc
Sergei <ip744692@yandex.ru>
2022-12-15 11:13:19 +0300

    Comment

---
+++ Seminar001_15.12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Возведение в Квадрат
+
+//Console.WriteLine("Введите число: ");
+//int a = int.Parse(Console.ReadLine()!);
+//int scr = a*a;                               // Математический метод
+//int scr1 = Convert.ToInt32(Math.Pow(a, 2));  // Использование функции с конвертацией 
+//Console.WriteLine($"Число {a} в квадрате является {scr} математически и {scr1} с использованием библиотеки");
+
+
+//
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = -a;
+while (b <= a) {
+    Console.Write($"{b} ");
+    b++;
+}
