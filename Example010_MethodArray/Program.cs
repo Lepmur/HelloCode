4af13881c8ac5389b41d92c1be9dd49fa4877ca8
Sergei <ip744692@yandex.ru>
2022-12-17 13:52:38 +0300

    add Lection 2 Example 008 - 011

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {14 ,22 ,31 ,44 ,53 ,64 ,73 ,84};
+int n = array.Length;
+int find = 44;
+int index = 0;
+
+while (index < n) {
+    if (array[index] == find) {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
