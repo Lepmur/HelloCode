eb39c1a1b672017e6f77fba53a740bbbe686eba2
Sergei <ip744692@yandex.ru>
2023-01-07 01:32:23 +0300

    add LeetCode 009

---
+++ Initiative009_LeetCode_Search_Insert_Position/Program.cs
@@ -0,0 +1,16 @@
+﻿int SearchInsert(int[] nums, int target) // функция возвращает индекс TARGET, а если его нет в массиве то возвращает место где он мог бы быть
+{
+    for (int i = 0; i < nums.Length; i++) // шагаем по массиву
+    {
+        if (nums[i] == target || nums[i] > target) // если текущий элемент >= TARGET возвращаем текущий индекс
+            return i;
+    }
+    return nums.Length; // если ничего не нашли, значи TARGET больше последнего элемента и его индекс на один больше индекса последнего элемента
+}
+
+
+int[] array = {1,3,5,6};
+
+System.Console.WriteLine(SearchInsert(array, 8)); 
+
+
