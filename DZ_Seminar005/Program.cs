f04fa4e5aed259122dc8b9f7421a04296861d196
Sergei <ip744692@yandex.ru>
2023-01-16 13:47:39 +0300

    add Seminar_5 and DZ_5

---
+++ DZ_Seminar005/Program.cs
@@ -0,0 +1,102 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArray(int size) // генерация массива размера size
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+    return array;
+}
+
+int CounterEven(int[] array) // счетчик четных чисел в array
+{
+    int count = 0;
+    foreach (var item in array)
+        if (item%2 == 0) count += 1;
+    return count;
+}
+
+string Answer(int[] array, int count) // формирование строки вывода
+{
+    string result = $"Сгенерировался массив трёхзначных чисел: {Environment.NewLine}"
+                  + $"[ {String.Join("; ", array)} ] {Environment.NewLine}"
+                  + $"Из них {count} четных!";
+    return result;
+}
+/*
+int[] numbers = CreateArray(10);
+int count = CounterEven(numbers);
+Console.Clear();
+System.Console.WriteLine(Answer(numbers, count));
+*/
+
+//=====================================================================
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+//                    [3, 7, 23, 12] -> 19
+//                    [-4, -6, 89, 6] -> 0
+
+int[] CreateArray1(int size) // генерация массива размера size
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 21);
+    return array;
+}
+
+int SumNotEven(int[] array) // считает сумму элементов с нечётной ПОЗИЦИЕЙ :-)
+{
+    int sum = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (i%2 == 0) sum += array[i];
+    }
+    return sum;
+}
+
+string Answer1(int[] array, int sum) // формирование строки вывода
+{
+    string result = $"Сгенерировался массив чисел: {Environment.NewLine}"
+                  + $"[ {String.Join("; ", array)} ] {Environment.NewLine}"
+                  + $"Сумма элементов на нечётных позициях = {sum}";
+    return result;
+}
+/*
+int[] numbers = CreateArray1(10);
+int sum = SumNotEven(numbers);
+Console.Clear();
+System.Console.WriteLine(Answer1(numbers, sum));
+*/
+
+// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний 
+// элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//                    [1 2 3 4 5] -> 5 8 3
+//                    [6 7 3 6] -> 36 21
+
+int[] SumCouple(int[] array)
+{
+    int j = array.Length - 1;
+    int[] result = new int[(int)Math.Round((double)array.Length / 2, MidpointRounding.ToPositiveInfinity)];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = (i == j) ? array[i] : array[i] + array[j];
+        j--;
+    }
+    return result;
+}
+
+string Answer3(int[] array, int[] sum) // формирование строки вывода
+{
+    string result = $"Сгенерировался массив чисел: {Environment.NewLine}"
+                  + $"[ {String.Join("; ", array)} ] {Environment.NewLine}"
+                  + $"Суммы противоположных элементов: {Environment.NewLine}"
+                  + $"[ {String.Join("; ", sum)} ]";
+    return result;
+}
+/*
+int[] numbers = CreateArray1(5); // тут использовал функцию из задачки выше чтобы не плодить копипаст
+int[] sum = SumCouple(numbers);
+Console.Clear();
+System.Console.WriteLine(Answer3(numbers, sum));
+*/
---
+++ Seminar005_16.01.23/Program.cs
@@ -0,0 +1,102 @@
+﻿//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+/*
+int[] array = GetArray(12,-9,9);
+Console.WriteLine($"[{String.Join(",", array)}]");
+int positiveSum = 0;
+int negativeSum = 0;
+foreach (int el in array)
+{
+    if(el > 0)
+        positiveSum += el;
+    else
+        negativeSum += el;
+}
+Console.WriteLine($"Сумма положительных равна {positiveSum}, сумма отрицательных равна {negativeSum}");
+*/
+// ------Метод------
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++){
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
+//=================== ЗАДАЧИ
+
+//Напишите программу замены элементов массива:  
+// положительные элементы замените на отрицательные 
+
+int[] GetArray2 (int size)
+{
+    int[] Array = new int[size];
+    for (int i =0; i < size; i++)
+      Array[i] = new Random().Next(-10, 11);
+    return Array; 
+}
+/*
+int[] Array = GetArray2(10); 
+Console.WriteLine($"[{String.Join(", ",Array)}]"); 
+for (int i = 0; i < Array.Length; i++) 
+{ 
+    Array[i] = Array[i] * (-1); 
+} 
+Console.WriteLine($"[{String.Join(", ",Array)}]");
+*/
+
+//  Задайте массив и напишите программу,
+// определяет, присутствует ли ли заданное число в массиве
+
+int[] GetArray1 (int size)
+{
+    int[] Array = new int[size];
+    for (int i =0; i < size; i++)
+    {
+      Array[i] = new Random().Next(-10, 11);
+    }
+    return Array;
+}
+/*
+int[] Array = GetArray1(10); 
+int Find = -5; 
+Console.WriteLine($"[{String.Join(", ",Array)}]"); 
+if (FindElement(Array, Find)) 
+    Console.WriteLine($"Данный элемент найден"); 
+else
+    Console.WriteLine($"Данный элемент не найден");
+*/
+bool FindElement (int[] Array, int Find) 
+{ 
+    foreach (int ArrayEl in Array) 
+    {
+        if (ArrayEl == Find) 
+            return true;
+    }
+    return false;
+} 
+
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+
+int[] GetArray3 (int size) 
+{ 
+    int[] Array = new int[size];
+    for (int i = 0; i < size; i++) 
+      Array[i] = new Random().Next(0, 1001);   
+    return Array; 
+} 
+/*
+int[] Array = GetArray3(123);
+Console.WriteLine($"[{String.Join(", ",Array)}]"); 
+int count = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i] >= 10 && Array[i] < 100) 
+        count = count + 1; 
+}
+Console.WriteLine($"Количество цифр в данном диапазоне {count}");
+*/
