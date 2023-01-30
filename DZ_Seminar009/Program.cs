bd1e359392fbacda372871708014727a347d9b66
Sergei <ip744692@yandex.ru>
2023-01-30 18:57:18 +0300

    add Seminar 9 & DZ 9

---
+++ DZ_Seminar009/Program.cs
@@ -0,0 +1,64 @@
+﻿// Выполнить с помощью рекурсии. Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+/*
+Console.Clear();
+Console.Write("Введи число: ");
+int n  = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(NumbersNto1(n));
+*/
+//=====================================================================
+// Выполнить с помощью рекурсии. Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+/*
+Console.Clear();
+Console.Write("Введи число M: ");
+int m  = int.Parse(Console.ReadLine()!);
+Console.Write("Введи число N: ");
+int n  = int.Parse(Console.ReadLine()!);
+
+if (m < n) Console.WriteLine(sumOfMtoN(m, n));
+else Console.WriteLine(sumOfMtoN(n, m));
+*/
+//=====================================================================
+// Выполнить с помощью рекурсии. Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9                   m = 3, n = 2 -> A(m,n) = 29
+/*
+Console.Clear();
+Console.Write("Введи число M: ");
+int m  = int.Parse(Console.ReadLine()!);
+Console.Write("Введи число N: ");
+int n  = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(mrAkkerman(m, n));
+*/
+//=====================================================================
+//============================МЕТОДЫ===================================
+//=====================================================================
+/*
+string NumbersNto1(int n)
+{
+    if (n == 1)
+        return n.ToString();
+    return (n + " " + NumbersNto1(n - 1));
+}
+
+int sumOfMtoN(int m, int n)
+{
+    if (m == n)
+        return m;
+    return m + sumOfMtoN(m + 1, n);
+    
+}
+
+int mrAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0)
+        return mrAkkerman(m - 1, 1);
+    else if (m > 0 && n > 0)
+        return mrAkkerman(m - 1, mrAkkerman(m, n - 1));
+    return 0; // возврат для некорректных (отрицательных) значениях m и n
+}
+*/
---
+++ Seminar009_30.01.23/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от 1 до N.
+/*
+Console.Write("Введи число: ");
+int N  = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PrintNumbers(1,N));
+
+string PrintNumbers(int start, int end)
+{
+    if(start == end) 
+        return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+*/
+//=====================================================================
+// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+/*
+string NaturalFromTo(int start, int end)
+{
+    if(start == end) 
+        return start.ToString();
+    return (start + " " + NaturalFromTo(start + 1, end));
+}
+
+Console.Clear();
+Console.Write("Введи число M: ");
+int m  = int.Parse(Console.ReadLine()!);
+Console.Write("Введи число N: ");
+int n  = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NaturalFromTo(m, n));
+*/
+//=====================================================================
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+/*
+int sumOfNumber(int number)
+{
+    if(number > 9) 
+        return number % 10 + sumOfNumber(number / 10);
+    return number;
+}
+
+Console.Clear();
+Console.Write("Введи число: ");
+int n  = int.Parse(Console.ReadLine()!);
+Console.WriteLine(sumOfNumber(n));
+*/
+//=====================================================================
+// Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+/*
+int Power(int a, int b)
+{
+    if (b > 1)
+        return a *= Power(a, b - 1);
+    return a;
+}
+
+Console.Clear();
+Console.Write("Введи число A: ");
+int a  = int.Parse(Console.ReadLine()!);
+Console.Write("Введи число B: ");
+int b  = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Power(a, b));
+*/
+//=====================================================================
+// Найдите факториал числа c помощью рекурсии
+/*
+int Fact(int a)
+{
+    if (a > 1)
+        return a * Fact(a - 1);
+    return a;
+}
+
+Console.Clear();
+Console.Write("Введи число A: ");
+int a  = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Fact(a));
+*/
